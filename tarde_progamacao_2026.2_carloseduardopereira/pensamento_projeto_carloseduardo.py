5ae2519522d401fe8d0e97fb315a238fbb78e81c
Carlos Pereira - Aluno vocação <carloseduardopereiramelo31@gmail.com>
2026-06-18 16:58:41 -0300

    Add files via upload

---
+++ tarde_progamacao_2026.2_carloseduardopereira/pensamento_projeto_carloseduardo.py
@@ -0,0 +1,55 @@
+'''
+>>projeto hamburgueria:
+
+'''
+
+# ufa, quebrei a maldição
+# print('Hello word!')
+
+print('-' * 48 + '\n')
+print('Bem vindo ao Sistem de vendas - Hamburgueria!\n')
+print('1 - Cadastrar produto')
+print('2 - Fale conosco')
+print('3 - sobre nós')
+print('4 - lanches')
+print('5 - combos disponíveis')
+print('6 - bebidas')
+print('7 - envie seu feedback')
+print('8 - forma de pagamento')
+print('9 - realizar compra')
+print('0 - sair do sistema')
+print('\n----------------------------------------------------------------------\n')
+
+opcao_definida = int(input('digite a opção desejada: '))
+
+if opcao_definida == 1:
+    print('cadastrando produto...')
+
+elif opcao_definida == 2:
+    print('fale conosco...')
+
+elif opcao_definida == 3:
+    print('sobre nós...')
+
+elif opcao_definida == 4:
+    print('lanches...')
+
+elif opcao_definida == 5:
+    print('combos...')
+
+elif opcao_definida == 6:
+    print('bebidas...')
+
+elif opcao_definida == 7:
+    print('envie seu feedback...')
+
+elif opcao_definida == 8:
+    print('formas de pagamentos...')
+
+elif opcao_definida == 9:
+    print('realizando compra...')
+
+elif opcao_definida == 0:
+    print('saindo do sistema...')
+
+
